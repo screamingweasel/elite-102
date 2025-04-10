b7686acea3a11d04ae9d4099a857ed56edbaf61f
James Barnett <jbarnett.s3its@gmail.com>
2025-04-10 14:52:21 -0700

    Added menu validation and create_customer

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import mysql.connector
+
+def get_connection():
+    connection = mysql.connector.connect(
+        host='localhost', # << running on the same machine
+        user = 'root',
+        password='root', # << whatever you set during mysql config
+        database='demo', # << your database name
+        autocommit=True) # << save changes as soon as made
+    return connection
+
 def show_main_menu():
     print("Menu:")
-    print("1.Create new customer")
+    print("1. Create new customer")
     print("2. Edit Customer")
     print("3. Delete Customer")
     print("x. Exit")
@@ -16,9 +27,47 @@ def edit_customer(customer_id):
 
     return
 
+
+def create_customer():
+    # Make sure that this is a new email address
+    while True:
+        email_address = input("Email Address: ")
+        if (get_customer_by_email(email_address) is not None):
+            print("That email is unavailable")
+            continue
+        else:
+            break
+        
+    customer_name = input("Customer Name: ")
+    credit_limit = int(input("Credit Limit: "))
+
+    customer = add_customer(customer_name, email_address, credit_limit)
+    input("Customer added. Press Enter to continue")
+
+
+def get_customer_by_email(email_address):
+    sql = "SELECT * FROM demo.customers where email_address = %s"
+    data = [email_address]
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(sql, data)
+    return cursor.fetchone()
+
+
+def add_customer(customer_name, email_address, credit_limit):
+    # Demonstrates using placeholders for variable and passing data as a List
+    sql = "INSERT INTO demo.customers (customer_name, email_address, credit_limit) values (%s, %s, %s)"
+    data = [customer_name, email_address, credit_limit]
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(sql, data)
+    customer_id = cursor.lastrowid # << This returns auto_increment value of inserted row
+    return customer_id
+
+
 def handle_choice(choice):
     if choice == '1':
-        print("Executing Option 1...")
+       create_customer()
     elif choice == '2':
         print("Executing Option 2...")
         edit_customer(1) # Need a customer #!!!
